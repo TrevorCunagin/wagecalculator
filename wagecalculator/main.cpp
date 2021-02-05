0466d1c609fae93225d12b30134a74c14be48351
Trevor Cunagin <73968417+TrevorCunagin@users.noreply.github.com>
2021-02-05 02:13:43 -0500

    Separated wage calculations into its own class

--- wagecalculator/main.cpp
+++ wagecalculator/main.cpp
@@ -7,8 +7,29 @@
 
 #include <cstdlib>
 #include <iostream>
+#include <iomanip>
 
 using namespace std;
+
+double calculatePay(double hours, double wages) {
+    double total; //gross wages for the week
+
+    //sets constant for the maximum hours worked before overtime
+    const double LIMIT = 40.0;
+
+    if (hours <= LIMIT) {
+        //regular wages for 40 hours or less
+        total = hours * wages;
+    }//end if statement
+    else //case for employee working overtime
+    {
+        //wages for overtime case, first 40 hours plus time and a half
+        total = (LIMIT * wages) + (hours - LIMIT)*(1.5 * wages);
+
+    } //end else
+
+    return total;
+}
 
 int main(int argc, char** argv) {
 
@@ -24,28 +45,14 @@ int main(int argc, char** argv) {
     cout << "Enter hours worked this week: ";
     cin >> hoursWorked; //get number of hours worked this week
 
-    //sets constant for the maximum hours worked before overtime
-    const double LIMIT = 40.0;
-
-    //gross wages
-    double wages;
-
-    if (hoursWorked <= LIMIT) {
-        //regular wages for 40 hours or less
-        wages = (hoursWorked * hourlyWage);
-    }//end if statement
-    else //case for employee working overtime
-    {
-        //wages for the first 40 hours
-        wages = LIMIT * hourlyWage;
-
-        //add overtime pay (time and a half)
-        wages += (hoursWorked - LIMIT)*(1.5 * hourlyWage);
-    } //end else
+    double totalWages = calculatePay(hoursWorked, hourlyWage);
+    
+    //specify output format
+    cout << fixed << setprecision(2);
 
     //display the gross wages
-    cout << "\nGross wages: $" << wages << endl << endl;
-
+    cout << "\nGross wages: $" << totalWages << endl << endl;
+    
     return 0;
 }
 
